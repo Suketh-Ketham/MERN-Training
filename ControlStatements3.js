b88b822f2fe93f2cfcef8bc0d90452df1746299c
Suketh-Ketham <73683420+Suketh-Ketham@users.noreply.github.com>
2026-01-29 22:27:09 +0530

    Delete ControlStatements3.js

--- ControlStatements3.js
+++
@@ -1,7 +0,0 @@
-let hasPaid = true
-let hasCompletedBasics = false
-let enrollMessage = (hasPaid && hasCompletedBasics)
-    ? "Enroll Now"
-    : "Complete Requirements";
-
-console.log(enrollMessage)
